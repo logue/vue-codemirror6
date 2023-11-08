49e7a564a629985d2613ef0e888193c008246e11
Logue <logue@hotmail.co.jp>
2023-11-08 14:40:20 +0900

    Fix for https://github.com/logue/vue-codemirror6/issues/19#issuecomment-1785637378

--- src/components/CodeMirror.ts
+++ src/components/CodeMirror.ts
@@ -219,7 +219,7 @@ export default defineComponent({
     linterConfig: {
       type: Object,
       default: () => {
-        return { needsRefresh: true };
+        return {};
       },
     },
     /**
