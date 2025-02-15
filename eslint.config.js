2b86b2c4da906f4591e13221e5c41e52b8792523
logue <logue@hotmail.co.jp>
2025-02-15 15:13:23 +0900

    Fixed types path. Update dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import configPrettier from '@vue/eslint-config-prettier';
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript';
 
 import pluginImport from 'eslint-plugin-import';
 import pluginVue from 'eslint-plugin-vue';
@@ -16,7 +19,7 @@ import pluginVueA11y from 'eslint-plugin-vuejs-accessibility';
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}']
+    files: ['**/*.{ts,mts,tsx,vue}'],
   },
   {
     name: 'app/files-to-ignore',
@@ -31,15 +34,16 @@ export default defineConfigWithVueTs(
       'playwright-report',
       'test-results',
       'public/',
-      'src/**/*.generated.*'
-    ]
+      'src/**/*.generated.*',
+      'docs',
+    ],
   },
   pluginVue.configs['flat/recommended'],
   ...pluginVueA11y.configs['flat/recommended'],
   vueTsConfigs.recommended,
   {
     plugins: {
-      import: pluginImport
+      import: pluginImport,
     },
 
     settings: {
@@ -47,7 +51,7 @@ export default defineConfigWithVueTs(
       'import/parsers': {
         espree: ['.js', '.cjs', '.mjs', '.jsx'],
         '@typescript-eslint/parser': ['.ts', '.tsx'],
-        'vue-eslint-parser': ['.vue']
+        'vue-eslint-parser': ['.vue'],
       },
       'import/resolver': {
         typescript: true,
@@ -55,14 +59,14 @@ export default defineConfigWithVueTs(
         'eslint-import-resolver-custom-alias': {
           alias: {
             '@': './src',
-            '~': './node_modules'
+            '~': './node_modules',
           },
-          extensions: ['.js', '.ts', '.jsx', '.tsx', '.vue']
-        }
+          extensions: ['.js', '.ts', '.jsx', '.tsx', '.vue'],
+        },
       },
       vite: {
-        configPath: './vite.config.ts'
-      }
+        configPath: './vite.config.ts',
+      },
     },
     rules: {
       // ...importPlugin.configs["recommended"].rules,
@@ -71,19 +75,22 @@ export default defineConfigWithVueTs(
       '@typescript-eslint/array-type': [
         'error',
         {
-          default: 'array'
-        }
+          default: 'array',
+        },
       ],
       // Enable @ts-ignore etc.
       '@typescript-eslint/ban-ts-comment': 'off',
       // Left-hand side style
-      '@typescript-eslint/consistent-generic-constructors': ['error', 'type-annotation'],
+      '@typescript-eslint/consistent-generic-constructors': [
+        'error',
+        'type-annotation',
+      ],
       // Enable import sort order, see bellow.
       '@typescript-eslint/consistent-type-imports': [
         'off',
         {
-          prefer: 'type-imports'
-        }
+          prefer: 'type-imports',
+        },
       ],
       // Fix for pinia
       '@typescript-eslint/explicit-function-return-type': 'off',
@@ -102,41 +109,49 @@ export default defineConfigWithVueTs(
       'import/order': [
         'error',
         {
-          groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+          groups: [
+            'builtin',
+            'external',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
           pathGroups: [
             // Vue Core
             {
               pattern:
                 '{vue,vue-router,vuex,@/store,vue-i18n,pinia,vite,vitest,vitest/**,@vitejs/**,@vue/**}',
               group: 'external',
-              position: 'before'
+              position: 'before',
             },
             // Internal Codes
             {
               pattern: '{@/**}',
               group: 'internal',
-              position: 'before'
-            }
+              position: 'before',
+            },
           ],
           pathGroupsExcludedImportTypes: ['builtin'],
           alphabetize: {
-            order: 'asc'
+            order: 'asc',
           },
-          'newlines-between': 'always'
-        }
+          'newlines-between': 'always',
+        },
       ],
       // A tag with no content should be written like <br />.
       'vue/html-self-closing': [
         'error',
         {
           html: {
-            void: 'always'
-          }
-        }
+            void: 'always',
+          },
+        },
       ],
       // Mitigate non-multiword component name errors to warnings.
-      'vue/multi-word-component-names': 'warn'
-    }
+      'vue/multi-word-component-names': 'warn',
+    },
   },
   configPrettier
 );
